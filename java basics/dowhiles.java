87928c6c0af77ba9905a669a58e3019bde6a49ba
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-04 14:43:17 +0530

    Updated

---
+++ java basics/dowhiles.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class dowhiles{
+
+public static void main(String[] args){
+
+int i=1;
+
+do{
+  System.out.println(" " +i);
+  i++;
+}while(i<5);
+}
+}
