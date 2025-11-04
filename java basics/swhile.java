7cdc1348bbacf5345b74effd3e6562118a1e68e3
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-04 14:23:47 +0530

    looping

---
+++ java basics/swhile.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class swhile{
+
+public static void main(String[] args){
+
+int count = 1;
+
+while(count <= 10){
+
+      System.out.println("hello " + count);
+      count++; 
+
+}
+}
+}
