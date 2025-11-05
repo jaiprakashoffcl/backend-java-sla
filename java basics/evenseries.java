8d0ed0d5e44ca4642e9e3a374e0348ec6b8af6a9
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-05 13:43:35 +0530

    updated

---
+++ java basics/evenseries.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class evenseries{
+
+public static void main(String[] args){
+
+         int count =0;
+         int limit = 25;
+         while(count<=limit){
+		System.out.println(count);
+		count+=2;
+         }
+}
+}
---
+++ java basics/sequence.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class sequence{
+  public static void main(String[] args){
+
+    int count = 1;
+    int limit = 14;
+
+    while(count <= limit){
+       if((count % 3) != 0){
+        System.out.println(count);
+    }
+    count += 1;
+}
+}
+}
+}
