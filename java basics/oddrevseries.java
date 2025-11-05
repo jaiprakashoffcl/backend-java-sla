334acea3c675bbccf4e1a04051fe9f2dbb6ae651
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-05 15:51:08 +0530

    series

---
+++ java basics/oddrevseries.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class oddrevseries{
+      public static void main(String[] args){
+          for(int i=1;i<=9;i+=2){
+             System.out.println(""+i);
+          }
+          for(int i=7;i>=1;i-=2){
+              System.out.println(""+i);
+          }
+}
+}
---
+++ java basics/series.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class series{
+  public static void main(String[] args){
+    
+     int n=5;
+     for(int i=1;i<=n;i++){
+          System.out.println(""+(i*i));
+          System.out.println(""+(i*i*i));
+}        
+}
+}
