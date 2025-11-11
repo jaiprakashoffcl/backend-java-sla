ab68894b01735b6c1240b779d06d1b45737dffe7
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-11 15:37:13 +0530

    new files

--- java basics/asc_order.java
+++ java basics/asc_order.java
@@ -17,7 +17,7 @@ public class asc_order{
             }
             
             for(int i =0 ;i<n;i++){
-                System.out.println(""+a[i]);
+                System.out.println(a[i]+"");
             }
       }
 }
