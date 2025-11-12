0ba059df8a9f81b0ca1dfe59008cbab0dae97e60
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-12 14:43:18 +0530

    star pattern programs

---
+++ java basics/onestar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class onestar{
+     public static void main(String []args){
+            
+            for(int i =1;i<=3;i++){
+               for(int j = 1;j<=3;j++){
+                   System.out.print("* ");
+               }
+              System.out.print("\n");
+            }
+     }
+}
