fcb47d77c93dc06480eca0e3c5dd38384e78b179
Jai Prakash <jaiprakash292033@gmail.com>
2025-11-24 09:09:52 +0530

    Squares from 1 to 5

---
+++ java-workout-interview/PrintSquares.java
@@ -0,0 +1,12 @@
+// print squares using multiplication of numbers from 1 to 5 (1-1, 2-4, 3-9, 4-16, 5-25)
+
+class PrintSquares
+{
+	public static void main (String[] args) 
+	{
+		for(int i=1;i<=5;i++){
+		    System.out.println(i+" - " +i*i);
+		}
+	}
+}
+
